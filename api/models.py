c375e8b126971fc7e306bd2d57f5736a483b78c4
angusmit <angusnsh@gmail.com>
2025-06-03 00:46:46 +0100

    Initial commit: Set up Django + React project

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    salary = models.IntegerField()
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Job
+from .serializers import JobSerializer
+
+class JobListCreate(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer

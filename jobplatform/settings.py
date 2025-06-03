a9c28179b0b3dd69e51daea7cb029b2ee64ad701
angusmit <angusnsh@gmail.com>
2025-06-04 00:46:21 +0100

    Adding User Authentication with Django REST Auth

--- jobplatform/settings.py
+++ jobplatform/settings.py
@@ -40,7 +40,22 @@ INSTALLED_APPS = [
     "api",
     "rest_framework",
     "corsheaders",
+    'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+# Add JWT settings
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
